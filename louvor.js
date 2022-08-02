f980d1396664ba9a8ba91b7590abc06a67055db5
Janyson22 <104655786+Janyson22@users.noreply.github.com>
2022-08-01 21:05:23 -0300

    Atualização do Projeto
    Segunda parte do Projeto

--- louvor.js
+++ louvor.js
@@ -1,26 +1,36 @@
-const modal = document.querySelector('#modalCantor')
-const tabela = document.getElementsByTagName('table')
+// const modal = document.querySelector('#modalCantor')
+// const tabela = document.getElementsByTagName('table')
 
-function adicionarCantor() {
-    let nomeCantor = document.getElementById('adicionarCantor').value
-    let lista = document.getElementById("lista").innerHtml
-    let listaAtual = `<tr><td>${nomeCantor}</td></tr>`
-    document.getElementById('lista').innerHTML = lista + listaAtual
+// function adicionarCantor() {
+//     let nomeCantor = document.getElementById('adicionarCantor').value
+//     let lista = document.getElementById("lista").innerHtml
+//     let listaAtual = `<tr><td>${nomeCantor}</td></tr>`
+//     document.getElementById('lista').innerHTML = lista + listaAtual
 
+// }
+
+// function linhas(){
+//     for (let i = 0; i < linhas.length; i++){
+//         let linha = linhas[i];
+//         linha.addEventListerner("click",function(){
+//             visualizarCantor();
+//         })
+//     }
+// }
+
+
+
+// function visualizarCantor(){
+//     modal.style.opacity = "1"
+// }
+
+function toggle() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+
+    let popup = document.getElementById('popup');
+    popup.classList.toggle('active')
 }
 
-function linhas(){
-    for (let i = 0; i < linhas.length; i++){
-        let linha = linhas[i];
-        linha.addEventListerner("click",function(){
-            visualizarCantor();
-        })
-    }
-}
 
-
-
-function visualizarCantor(){
-    modal.style.opacity = "1"
-}
 
